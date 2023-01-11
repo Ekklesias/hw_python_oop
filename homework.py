276675625e9726d3fb44124e78bfee38c0293f62
Сергий Яковлев <neznaikaknazvatsy@yandex.ru>
2023-01-12 01:14:02 +0300

    fix6

--- homework.py
+++ homework.py
@@ -10,11 +10,11 @@ class InfoMessage:
     distance: float
     speed: float
     calories: float
-    full_message: str = ('Тип тренировки: {training_type};'
-                         ' Длительность: {duration:.3f} ч.; '
-                         ' Дистанция: {distance:.3f} км; '
-                         ' Ср. скорость: {speed:.3f} км/ч; '
-                         ' Потрачено ккал: {calories:.3f}.')
+    full_message: str = ('Тип тренировки: {training_type}; '
+                         'Длительность: {duration:.3f} ч.; '
+                         'Дистанция: {distance:.3f} км; '
+                         'Ср. скорость: {speed:.3f} км/ч; '
+                         'Потрачено ккал: {calories:.3f}.')
 
     def get_message(self) -> str:
         return self.full_message.format(**asdict(self))
